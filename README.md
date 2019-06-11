34debcb02576c015c639a58846a0dce2cdeee0eb
Mayja Tom <maja.morales17@gmail.com>
2019-06-10 23:43:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Professional-Site
+Updated professional information with personal projects
---
+++ js/grayscale.js
@@ -0,0 +1,74 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  //Any Chart for time spent
+  anychart.onDocumentReady(function () {
+    var chart = anychart.pie([
+        {x: "Building Apps", value: 10},
+        {x: "Kayaking", value: 12},
+        {x: "Playing Piano", value: 5},
+        {x: "Learning new coding languages", value: 18},
+        {x: "Studying Reiki", value: 9}
+    ]);
+
+    chart.innerRadius("55%");
+
+    var label = anychart.standalones.label();
+    label.text("Activities");
+    label.width("100%");
+    label.height("100%");
+    label.adjustFontSize(true);
+    label.fontColor("#60727b");
+    label.hAlign("center");
+    label.vAlign("middle");
+
+    // set the label as the center content
+    chart.center().content(label);
+
+    //adding monochrome color palette
+    chart.palette(anychart.palettes.turquoise);
+
+
+    chart.container("container");
+    chart.draw();
+});
+
+})(jQuery); // End of use strict
